febccd7e8ff975d2143e48beb5a8e0a804e535dc
Robert Reyes <neorob22@gmail.com>
2023-02-04 23:56:38 -0600

    started react app with 2 routes and basic styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import { AppBar, Box, Button, Container, Menu, Toolbar, Tooltip, Typography } from '@mui/material';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Landing from './landing/Landing';
+import Product from './products/Product';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Landing />
+    },
+    {
+      path: "/products",
+      element: <Product />
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <AppBar position="static">
+        <Container maxWidth="x1">
+          <Toolbar disableGutters>  
+              {/*Logo title section*/}
+              <Typography
+              variant="h6"
+              noWrap
+              component="a"
+              href="/"
+              sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+              }}>MicroShop</Typography>
+
+              {/*Menu options section*/}
+              <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                <Button href="/" sx={{ my: 2, color: 'white', display: 'block' }}>Home</Button>
+                <Button href="/products" sx={{ my: 2, color: 'white', display: 'block' }}>Products</Button>
+              </Box>
+
+              {/*User account section*/}
+              <Box sx={{ flexGrow: 0 }}>
+                <Tooltip title="Settings">
+                <Button sx={{ my: 2, color: 'white', display: 'block' }}>Settings</Button>
+                </Tooltip>
+                <Menu
+                sx={{ mt: '45px' }}
+                id="menu-appbar"
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                >
+                  <Button sx={{ my: 2, color: 'white', display: 'block' }}>Log In</Button>
+                </Menu>
+              </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/landing/Landing.js
@@ -0,0 +1,7 @@
+function Landing(){
+    return(
+        <h1>Landing page</h1>
+    )
+}
+
+export default Landing;
